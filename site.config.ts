7e80ff192e58e64c5cab7c23e102afa85b6afbad
Cypherleg <130465103+Cypherleg@users.noreply.github.com>
2025-12-24 17:27:58 -0300

    Atualizar o site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '2d226bc6ed36801a83d5e9fd1bc2dad6',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
