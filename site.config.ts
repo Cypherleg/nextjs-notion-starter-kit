58c3a3ba28602543c90b074385dee0f1db77a481
Cypherleg <130465103+Cypherleg@users.noreply.github.com>
2025-12-24 21:56:29 -0300

    Atualizar o site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -51,7 +51,7 @@ export default siteConfig({
   '/oftalmologista-umuarama': 'ID_DA_PAGINA_NOTION',
   '/cirurgia-refrativa': 'ID_DA_PAGINA_NOTION',
   '/cirurgia-de-catarata': 'ID_DA_PAGINA_NOTION',
-  '/contato': 'ID_DA_PAGINA_NOTION'
+  '/contato': '2d226bc6ed36801a83d5e9fd1bc2dad6'
 }
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
